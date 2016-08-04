60733e811a23f3a55245485d531ffb81163f5d1f
NaokiShizuka <naokishizuka14@gmail.com>
2016-08-05 00:28:19 +0900

    マルチドメイン対策を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,26 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+  skip_before_filter :verify_authenticity_token
+
+
+  before_action :add_allow_credentials_headers
+
+  def add_allow_credentials_headers
+    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS#section_5
+    #
+    # Because we want our front-end to send cookies to allow the API to be authenticated
+    # (using 'withCredentials' in the XMLHttpRequest), we need to add some headers so
+    # the browser will not reject the response
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  end
+
+  def options
+    head :status => 200, :'Access-Control-Allow-Headers' => 'accept, content-type'
+  end
+
 end
